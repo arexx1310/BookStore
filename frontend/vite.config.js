9b0b6c047bd97a7ef37a1d9639e3683301b29f11
Archi Danva <archidanva@gmail.com>
2025-11-26 00:53:20 +0530

    Working INventory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,11 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bookstore-1qov.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   plugins: [
     react(),
     tailwind()
   ]
 })
+
+
+//changes
